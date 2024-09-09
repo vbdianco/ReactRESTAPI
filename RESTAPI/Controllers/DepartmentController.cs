d17b2e73b07c4eedb62db717d188715e70db9fc0
vincedianco <vbdianco@gmail.com>
2024-09-09 15:06:48 +0800

    Commit React JS Frontend and REST API files
    Commit React JS Frontend and REST API files

---
+++ RESTAPI/Controllers/DepartmentController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using RESTAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RESTAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public JsonResult Get()
+        {
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand("SelectAllDepartments"))
+                {
+                    myCommand.CommandType = CommandType.StoredProcedure;
+                    myCommand.Connection = myCon;
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+
+                return new JsonResult(table);
+
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public JsonResult Post(Department dep)
+        {
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand("InsertDepartment"))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentName", dep.DepartmentName);
+                    myCommand.CommandType = CommandType.StoredProcedure;
+                    myCommand.Connection = myCon;
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+
+                return new JsonResult("Added Successfully");
+
+            }
+        }
+
+        // PUT api/values/5
+        [HttpPut]
+        public JsonResult Put(Department dep)
+        {
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand("UpdateDepartment"))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentId", dep.DepartmentId);
+                    myCommand.Parameters.AddWithValue("@DepartmentName", dep.DepartmentName);
+                    myCommand.CommandType = CommandType.StoredProcedure;
+                    myCommand.Connection = myCon;
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+
+                return new JsonResult("Update Successfully");
+
+            }
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand("DeleteDepartment"))
+                {
+                    myCommand.Parameters.AddWithValue("@DepartmentId", id);
+                    myCommand.CommandType = CommandType.StoredProcedure;
+                    myCommand.Connection = myCon;
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+
+                return new JsonResult("Deleted Successfully");
+
+            }
+        }
+    }
+}
---
+++ react-app/src/Home.js
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+export class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h3>This is a sample React JS App with .Net REST API and Azure SQL Backend deployed in Microsoft Azure</h3>
+            </div>
+        )
+    }
+}
